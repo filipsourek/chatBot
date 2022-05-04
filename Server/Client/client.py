c62380a2894bcaca90dae6daf6d4376fff33f145
filipsourek <filip.sourek@tul.cz>
2022-05-05 00:15:17 +0200

    Update client.py

--- Server/Client/client.py
+++ Server/Client/client.py
@@ -4,3 +4,22 @@ IP = "194.233.160.94"
 
 PORT = 5555 
 HEADERSIZE = 5
+
+class Client:
+    def __init__(self):
+        self.client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.status = "Disconected"
+        try:  
+            self.client.connect((IP,PORT)) 
+            self.status = "Connected"
+            
+        except socket.error:
+            self.status = "Disconected"
+    def send_msg(self, msg):
+        if msg:
+            msg = msg.encode()
+            msg_head = f"{len(msg):<{HEADERSIZE}}".encode()
+            try:
+                self.client.send(msg_head + msg)     
+            except socket.error:
+                self.status = "Disconected"
