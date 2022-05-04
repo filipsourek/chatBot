cf4a2f8595534e2adbd9d3a1db7f17bbe25c1d85
filipsourek <filip.sourek@tul.cz>
2022-05-04 23:51:06 +0200

    Add basic methods for bot

--- Server/bot.py
+++ Server/bot.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 
 
 URL = "https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt"
@@ -6,3 +7,8 @@ class Bot:
     def __init__(self):
         self.name = "Eduard"
         self.values = dict()
+
+    def getTime(self):
+        return datetime.now().strftime("%X")
+    def getName(self):
+        return self.name
