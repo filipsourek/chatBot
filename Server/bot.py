154b0efa4549c335baf2f21fa816a4d9d216bf00
filipsourek <filip.sourek@tul.cz>
2022-05-04 23:48:09 +0200

    Create init bot.py

---
+++ Server/bot.py
@@ -0,0 +1,8 @@
+
+
+URL = "https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt"
+
+class Bot:
+    def __init__(self):
+        self.name = "Eduard"
+        self.values = dict()
